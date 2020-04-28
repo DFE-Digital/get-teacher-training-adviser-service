00bd6c6f15e5262da8512ddf739a7091a93fc865
jebw <jeb@jdwilkins.co.uk>
2020-04-28 17:56:01 +0100

    Restore rails default exception handler in development and test environments

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,5 @@ module GetIntoTeachingRegister
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    config.exceptions_app = routes
   end
 end
