2fc20a782ff26b1a3f166b042cf120773c2dced3
jebw <jeb@jdwilkins.co.uk>
2020-05-05 13:53:18 +0100

    Fixed up path in test

--- spec/features/view_pages_spec.rb
+++ spec/features/view_pages_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.feature "View pages", type: :feature do
   scenario "Navigate to home" do
-    visit "/pages/home"
+    visit "/home"
 
     expect(page).to have_text("Lorem")
   end
