c29e649caa1587870fece2d3d1f04ac203d955ff
maxmills <8balldigitalsolutions@gmail.com>
2020-05-14 19:17:36 +0100

    basic identity tests

--- spec/models/identity_spec.rb
+++ spec/models/identity_spec.rb
@@ -16,5 +16,31 @@ RSpec.describe 'Identity' do
         expect(no_name.errors[:first_name]).to eq ["can't be blank"]
       end
     end
+
+    context "with invalid email addresses" do
+      ['test.com', 'test@@test.com', 'FFFF', 'test@test', 'test@test.'].each do |invalid_email_address|
+        let(:instance) { build(:identity, email_address: invalid_email_address) }
+        it "should not be valid" do
+          expect(instance).to_not be_valid
+        end
+      end
+    end
+
+    context "with valid email addresses" do
+      ['test@example.com', 'testymctest@gmail.com', 'test%.mctest@domain.co.uk'].each do |valid_email_address|
+        let(:instance) { build(:identity, email_address: valid_email_address) }
+        it "should be valid" do
+          expect(instance).to be_valid
+        end
+      end
+    end
+
   end
+
+  describe '#next_step' do
+    it "should return the next step" do
+      expect(identity.next_step).to eq('returning_teacher')
+    end
+  end
+
 end
