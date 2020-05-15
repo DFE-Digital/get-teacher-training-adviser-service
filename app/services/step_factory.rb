516241c3adcf9134e18fda035641c479949ec3ff
maxmills <8balldigitalsolutions@gmail.com>
2020-05-15 12:28:54 +0100

    more StepFactory tests

--- app/services/step_factory.rb
+++ app/services/step_factory.rb
@@ -1,7 +1,25 @@
 class StepFactory
-# need to register permitted classes
+  # need to register permitted names/classes
+  STEP_NAMES = %w( Identity ReturningTeacher PrimaryOrSecondary ).freeze
+
+  class NameNotFoundError < StandardError
+
+    attr_reader :name
+
+    def initialize(name)
+      super
+      @name = name
+    end
+
+    def message
+      "Step name not found for #{name}"
+    end
+
+  end
+
   def self.create(name)
     classified_name = name.camelize
+    raise NameNotFoundError.new(classified_name) unless STEP_NAMES.include?(classified_name)
     Object.const_get(classified_name).new
   end
   
--- spec/services/step_factory_spec.rb
+++ spec/services/step_factory_spec.rb
@@ -6,16 +6,18 @@ RSpec.describe StepFactory do
   describe "#create" do
     context "with a valid constant" do
       it "instantiates the class" do
-        expect(subject.create("Identity")).to be_an_instance_of(Identity)
+        expect(subject.create("identity")).to be_an_instance_of(Identity)
       end
     end
 
     context "with an invalid constant" do
-      it "raises a NameError" do
-        expect { subject.create("Invalid") }.to raise_error(NameError)
+      it "raises a custom NameNotFoundError" do
+        expect { subject.create("Invalid") }.to raise_error { |error|
+        expect(error).to be_a(StepFactory::NameNotFoundError)
+        expect(error.message).to eq("Step name not found for Invalid")
+      }
       end
     end
-    
   end
   
 end
