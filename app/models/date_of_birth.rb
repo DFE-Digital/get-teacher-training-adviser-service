280a1d06f68a4625c23248c9a62ae47190926052
maxmills <8balldigitalsolutions@gmail.com>
2020-05-19 09:45:28 +0100

    custom date of birth validation

--- app/models/date_of_birth.rb
+++ app/models/date_of_birth.rb
@@ -1,5 +1,16 @@
 class DateOfBirth < Base
+
   attribute :date_of_birth, :datetime
+
+  validate :validate_date
+
+  def validate_date
+    begin
+      self.date_of_birth = Date.parse(self.date_of_birth)
+    rescue
+      errors.add(:date_of_birth, 'Date does not exist. Please insert valid date')
+    end
+  end
   
   def next_step
    "uk_or_overseas" 
@@ -12,18 +23,32 @@ class DateOfBirth < Base
   end
 
   def assign_attributes(attributes)
-    new_attributes            = attributes.stringify_keys
+    new_attributes = attributes.stringify_keys
     multiparameter_attributes = extract_multiparameter_attributes(new_attributes)
 
     multiparameter_attributes.each do |multiparameter_attribute, values_hash|
       set_values = (1..3).collect{ |position| values_hash[position].to_i }
-
+   
+    if set_values.include?(0)
+      errors.add(:date_of_birth, "can't be blank") 
+      return
+    elsif set_values[0] < 1920 || set_values[0] > Date.today.year
+      errors.add(:date_of_birth, "not a valid year")
+      return
+    elsif set_values[1] < 1 || set_values[1] > 12
+      errors.add(:date_of_birth, "not a valid month")
+      return
+    elsif set_values[2] < 1 || set_values[2] > 31
+      errors.add(:date_of_birth, "not a valid day")
+      return
+    end
+      
       self.send("#{multiparameter_attribute}=", Date.new(*set_values))
     end
   end
 
   protected
-  
+
   def extract_multiparameter_attributes(new_attributes)
     multiparameter_attributes = []
 
@@ -41,7 +66,7 @@ class DateOfBirth < Base
 
     pairs.each do |pair|
       multiparameter_name, value = pair
-      attribute_name             = multiparameter_name.split('(').first
+      attribute_name = multiparameter_name.split('(').first
       attributes[attribute_name] = {} unless attributes.include?(attribute_name)
 
       attributes[attribute_name][find_parameter_position(multiparameter_name)] ||= value
