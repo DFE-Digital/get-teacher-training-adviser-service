12997f54a8399fdbf2006bbae3987b691033a4a8
maxmills <8balldigitalsolutions@gmail.com>
2020-05-27 12:05:04 +0100

    simple sitemap

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,14 +1,11 @@
 class SitemapsController < ApplicationController
   skip_before_action :http_basic_authenticate
 
-  
-
   def index
     @host = "#{request.protocol}#{request.host}"
     @steps = StepFactory::STEP_NAMES
     respond_to do |format|
       format.xml
-      format.html 
     end
   end
 end
--- spec/requests/sitemaps_spec.rb
+++ spec/requests/sitemaps_spec.rb
@@ -1,10 +1,12 @@
 require "rails_helper"
 
-RSpec.describe SitemapsController, type: :controller do
+RSpec.describe SitemapsController, type: :request do
+  
   describe "GET #sitemap" do
     it "returns success" do
-      get :index
+      get :index, :format => :xml
       expect(response).to have_http_status(:success)
+      expect(response).to have_tag("loc")
     end
   end
 
