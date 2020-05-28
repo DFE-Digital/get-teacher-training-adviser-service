8153c8eba24922fcac48cebb4ffb9fd5526c9ecd
maxmills <8balldigitalsolutions@gmail.com>
2020-05-28 15:53:54 +0100

    starting modules

---
+++ app/models/degree.rb
@@ -0,0 +1,3 @@
+module Degree
+  
+end
---
+++ app/models/degree/what_subject_degree.rb
@@ -0,0 +1,11 @@
+module Degree
+  class WhatSubjectDegree < Base
+    attribute :degree_subject, :string
+    
+    validates :degree_subject, inclusion: { in: %w(Maths History English French), message: "Please select an option."}
+
+    def next_step
+      "stage_interested_teaching"
+    end
+  end
+end
