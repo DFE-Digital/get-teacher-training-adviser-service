da412aa3c027e4a0fd92dde565dbfe0fc6aed74e
maxmills <8balldigitalsolutions@gmail.com>
2020-05-29 16:55:01 +0100

    removed skip authenticate for sitemap

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,6 +1,4 @@
 class SitemapsController < ApplicationController
-  skip_before_action :http_basic_authenticate # needed for google?
-
   def index
     @host = "#{request.protocol}#{request.host}"
     @steps = StepFactory::STEP_NAMES
