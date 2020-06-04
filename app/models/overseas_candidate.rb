bd629ecb9d537b4dfd877d2da4b6bec9b4b40bac
maxmills <8balldigitalsolutions@gmail.com>
2020-06-04 15:30:51 +0100

    telephone max and min message

--- app/models/overseas_candidate.rb
+++ app/models/overseas_candidate.rb
@@ -3,7 +3,7 @@ class OverseasCandidate < Base
   attribute :callback_date, :string
   attribute :callback_time, :string 
 
-  validates :telephone_number, length: { minimum: 5, message: "Telephone number is too short" }, format: { with: /\A[0-9\s]+\z/, message: "Telephone number must consist of numbers only" }
+  validates :telephone_number, length: { in: 5..20, too_short: "Telephone number is too short", too_long: "Telephone number is too long (maximum is 20 characters)" }, format: { with: /\A[0-9\s]+\z/, message: "Telephone number must consist of numbers only" }
   validates :callback_date, presence: { message: "You need to complete this field" }
   validates :callback_time, presence: { message: "You need to complete this field" }
 
