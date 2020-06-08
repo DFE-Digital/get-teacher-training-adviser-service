b14c079be5f2f3896a8e47572a7e03d85efc5f28
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2020-06-08 16:13:06 +0100

    update subject list

--- app/models/subject_interested_teaching.rb
+++ app/models/subject_interested_teaching.rb
@@ -1,7 +1,38 @@
 class SubjectInterestedTeaching < Base
   attribute :teaching_subject, :string
   
-  validates :teaching_subject, inclusion: { in: %w(Maths History English French), message: "Please select an option"}
+  validates :teaching_subject, inclusion: { in: [
+    'Art and design', 
+    'Biology',
+    'Business studies', 
+    'Chemistry',
+    'Citizenship', 
+    'Classics',
+    'Computing', 
+    'Dance',
+    'Design and technology',
+    'Drama',
+    'Economic', 
+    'English',
+    'French',
+    'Geography', 
+    'German',
+    'Health and social care', 
+    'History',
+    'Languages (other)', 
+    'Maths',
+    'Media studies', 
+    'French',
+    'Music',
+    'Physical education', 
+    'Physics',
+    'Physics with maths',
+    'Primary psychology',
+    'Religious education',
+    'Social sciences',
+    'Spanish',
+    'Vocational health'] 
+  }
 
   def next_step
     "start_teacher_training"
--- spec/models/what_subject_degree_spec.rb
+++ spec/models/what_subject_degree_spec.rb
@@ -15,7 +15,37 @@ RSpec.describe WhatSubjectDegree do
     end
 
     context "with valid subject options" do
-      ['English', 'History', 'French', 'Maths'].each do |valid_subject|
+      [
+        'Art and design', 
+        'Biology',
+        'Business studies', 
+        'Chemistry',
+        'Citizenship', 
+        'Classics',
+        'Computing', 
+        'Dance',
+        'Design and technology',
+        'Drama',
+        'Economic', 
+        'English',
+        'French',
+        'Geography', 
+        'German',
+        'Health and social care', 
+        'History',
+        'Languages (other)', 
+        'Maths',
+        'Media studies', 
+        'French',
+        'Music',
+        'Physical education', 
+        'Physics',
+        'Physics with maths',
+        'Primary psychology',
+        'Religious education',
+        'Social sciences',
+        'Spanish',
+        'Vocational health'].each do |valid_subject|
         let(:instance) { build(:what_subject_degree, degree_subject: valid_subject) }
         it "is valid" do
           expect(instance).to be_valid
