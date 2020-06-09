b62e767a38f09d078ccbcb80a7f811589b1adb23
maxmills <8balldigitalsolutions@gmail.com>
2020-06-09 16:56:55 +0100

    10 year limit for callbacks

--- app/models/overseas_candidate.rb
+++ app/models/overseas_candidate.rb
@@ -13,7 +13,7 @@ class OverseasCandidate < Base
   validates :callback_date, presence: { message: "You need to enter a valid date" }
   validates :callback_time, presence: { message: "You need to complete this field" }
 
-  validate :callback_date_cannot_be_in_past
+  validate :callback_date_cannot_be_in_past, :callback_date_limit
 
   def callback_date_cannot_be_in_past
     if callback_date.present? && callback_date < Date.today
@@ -21,19 +21,22 @@ class OverseasCandidate < Base
     end
   end
 
+  def callback_date_limit
+    if callback_date.present? && callback_date > Date.today.next_year(10)
+      errors.add(:callback_date, "Date must be within 10 years")
+    end
+  end
+
   def make_a_date
     year = self.send("callback_date(1i)").to_i 
     month = self.send("callback_date(2i)").to_i
     day = self.send("callback_date(3i)").to_i
-  
-    return if year < Date.today.year || year > Date.today.next_year(10).year
     
     begin # catch invalid dates, e.g. 31 Feb
       self.callback_date = Date.new(year, month, day)
     rescue ArgumentError
       return
     end
-
   end
 
   def next_step
