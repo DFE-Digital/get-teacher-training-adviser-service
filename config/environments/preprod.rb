6c1cddc0c1b0d548a70d0cd0cdd7d1e01763df57
maxmills <8balldigitalsolutions@gmail.com>
2020-06-11 23:29:59 +0100

    redis in preprod environment

--- config/environments/preprod.rb
+++ config/environments/preprod.rb
@@ -3,4 +3,13 @@ require File.expand_path("production.rb", __dir__)
 
 Rails.application.configure do
   # Override any production defaults here
+  # Use a different cache store in preprod.
+  redis_url = JSON.parse(ENV['VCAP_SERVICES'])[:redis][0][:credentials][:uri]
+  config.cache_store = :redis_cache_store, { 
+    url: redis_url,
+    reconnect_attempts: 1,
+    error_handler: -> (method:, returning:, exception:) {
+    # report errors
+    } 
+  }
 end
