b195ec0466e629fc2d1683ea7cbcfc28fb8f2d83
maxmills <8balldigitalsolutions@gmail.com>
2020-06-11 11:28:58 +0100

    first attempt to set redis_url

--- config/environments/rolling.rb
+++ config/environments/rolling.rb
@@ -3,4 +3,7 @@ require File.expand_path("production.rb", __dir__)
 
 Rails.application.configure do
   # Override any production defaults here
+  # Use a different cache store in production.
+  redis_url = JSON.parse(ENV['VCAP_SERVICES'])[:redis][0][:credentials][:uri]
+  config.cache_store = :redis_cache_store { url: redis_url }
 end
