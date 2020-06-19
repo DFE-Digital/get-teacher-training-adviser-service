7d8ae3cfa8376628b5c804731efa46ad54688272
maxmills <8balldigitalsolutions@gmail.com>
2020-06-19 10:27:39 +0100

    start_teacher_training refactor

--- app/models/start_teacher_training.rb
+++ app/models/start_teacher_training.rb
@@ -1,14 +1,18 @@
 class StartTeacherTraining < Base
   attribute :year_of_entry, :string
 
-  validates :year_of_entry, inclusion: { in: %w(2020 2021 2022) }
+  validate :date_cannot_be_in_the_past
   
   def year_range(number_of_years) # sets year range for view
-    years = []
-    (Date.today.year..Date.today.next_year(number_of_years).year).to_a.each do |year|
-      years << OpenStruct.new(value: year, name: year)
+    (Date.today.year..Date.today.next_year(number_of_years).year).map do |year|
+      OpenStruct.new(value: year, name: year)
     end
-    years
+  end
+
+  def date_cannot_be_in_the_past
+    if year_of_entry.present? && year_of_entry.to_i < Date.today.year
+      errors.add(:year_of_entry, "Date can't be in the past")
+    end
   end
 
   def next_step
