2784c163eec85870abb78daca097d43a9e051cfd
maxmills <8balldigitalsolutions@gmail.com>
2020-06-24 13:30:38 +0100

    review corrections and time zone for overseas candidates

--- spec/factories/overseas_candidate.rb
+++ spec/factories/overseas_candidate.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     telephone_number { "12345678" }
     callback_date { Date.tomorrow }
     callback_time { "1100" }
+    time_zone { "Hawaii" }
   end
 end
--- spec/models/what_subject_degree_spec.rb
+++ spec/models/what_subject_degree_spec.rb
@@ -5,52 +5,7 @@ RSpec.describe WhatSubjectDegree do
   let(:wrong_answer) { build(:what_subject_degree, degree_subject: "dont know") }
 
   describe "validation" do
-    context "with invalid subject options" do
-      ['skiing', 'fishing', 'golfing', 'surfing' ].each do |invalid_subject|
-        let(:instance) { build(:what_subject_degree, degree_subject: invalid_subject) }
-        it "is not valid" do
-          expect(instance).to_not be_valid
-        end
-      end
-    end
-
-    context "with valid subject options" do
-      [
-        'Art and design', 
-        'Biology',
-        'Business studies', 
-        'Chemistry',
-        'Citizenship', 
-        'Classics',
-        'Computing', 
-        'Dance',
-        'Design and technology',
-        'Drama',
-        'Economic', 
-        'English',
-        'French',
-        'Geography', 
-        'German',
-        'Health and social care', 
-        'History',
-        'Languages (other)', 
-        'Maths',
-        'Media studies', 
-        'French',
-        'Music',
-        'Physical education', 
-        'Physics',
-        'Physics with maths',
-        'Primary psychology',
-        'Religious education',
-        'Social sciences',
-        'Spanish',
-        'Vocational health'].each do |valid_subject|
-        it "is valid" do
-          expect(build(:what_subject_degree, degree_subject: valid_subject)).to be_valid
-        end
-      end
-    end
+    xit "will do something when we have the api" 
   end
 
   describe "#next_step" do
