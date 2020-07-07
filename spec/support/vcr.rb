5bd041d3e5785a4880c3d5f2cf8a8cf295faece0
maxmills <8balldigitalsolutions@gmail.com>
2020-07-07 09:45:31 +0100

    updated vcr cassettes

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,7 +2,7 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/vcr"
   config.hook_into :typhoeus
   config.configure_rspec_metadata!
-  Rails.application.credentials.config.each do |k,v|
+  Rails.application.credentials.config.each do |k, v|
     config.filter_sensitive_data("ENV[#{k}]") { v }
   end
 end
