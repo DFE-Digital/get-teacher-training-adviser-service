40180b1525db1d183de8ca07f5ba9cb3cc7f8106
maxmills <8balldigitalsolutions@gmail.com>
2020-07-16 11:46:31 +0100

    update api client config

--- config/initializers/get_into_teaching_api_client.rb
+++ config/initializers/get_into_teaching_api_client.rb
@@ -1,9 +1,9 @@
 GetIntoTeachingApiClient.configure do |config|
-  endpoint = ENV["GIT_API_ENDPOINT"] || Rails.application.config.x.git_api_endpoint.presence
+  endpoint = ENV["GIT_API_ENDPOINT"].presence || Rails.application.config.x.git_api_endpoint.presence
   if endpoint
     parsed = URI.parse(endpoint)
     config.host = parsed.hostname
   end
 
-  config.api_key["Authorization"] = ENV["GIT_API_TOKEN"] || Rails.application.credentials.config[:api_key]
+  config.api_key["Authorization"] = ENV["GIT_API_TOKEN"].presence || Rails.application.credentials.config[:api_key].presence
 end
