098b00a9b0ebce49f48bdf3cb1ea2d7ad5ec63db
maxmills <8balldigitalsolutions@gmail.com>
2020-07-20 09:46:48 +0100

    stage of degree working

--- app/models/have_a_degree.rb
+++ app/models/have_a_degree.rb
@@ -7,16 +7,16 @@ class HaveADegree < Base
   OPTIONS = { yes: "222750000", no: "222750004", studying: "studying", equivalent: "222750005" }.freeze
   DEGREE_TYPE = { degree: "222750000", equivalent: "222750005" }.freeze
 
-  validates :degree, inclusion: { in: OPTIONS.map{ |k,v| v }, message: "Select an option from the list" }
+  validates :degree, inclusion: { in: OPTIONS.map { |_k, v| v }, message: "Select an option from the list" }
   validates :degree_type, types: { method: :get_qualification_degree_status }
 
   def set_degree_type
-    case degree
-    when OPTIONS[:equivalent]
-      self.degree_type = DEGREE_TYPE[:equivalent]
-    else
-      self.degree_type = DEGREE_TYPE[:degree]
-    end
+    self.degree_type = case degree
+                       when OPTIONS[:equivalent]
+                         DEGREE_TYPE[:equivalent]
+                       else
+                         DEGREE_TYPE[:degree]
+                       end
   end
 
   def next_step
--- app/models/studying/stage_of_degree.rb
+++ app/models/studying/stage_of_degree.rb
@@ -10,6 +10,11 @@ module Studying
       generate_api_options(ApiClient::get_qualification_degree_status)
     end
 
+    #  final_year: "222750001"
+    #  second_year: "222750002"
+    #  first_year: "222750003"
+    #  other: "222750005"
+
     def next_step
       "studying/what_subject_degree"
     end
