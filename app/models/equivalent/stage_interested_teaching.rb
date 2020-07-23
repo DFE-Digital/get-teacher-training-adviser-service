50e244d10b4b38793aaaf5cea1e3b29bc9ac0c9d
maxmills <8balldigitalsolutions@gmail.com>
2020-07-23 11:50:22 +0100

    update equivalent flow

--- app/models/equivalent/stage_interested_teaching.rb
+++ app/models/equivalent/stage_interested_teaching.rb
@@ -1,7 +1,11 @@
 module Equivalent
   class Equivalent::StageInterestedTeaching < StageInterestedTeaching
     def next_step
-      "equivalent/subject_interested_teaching"
+      if preferred_education_phase_id == StageInterestedTeaching::OPTIONS[:primary]
+        "equivalent/start_teacher_training"
+      else
+        "equivalent/subject_interested_teaching"
+      end
     end
   end
 end
