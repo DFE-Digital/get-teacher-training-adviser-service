6f988053b4b765d280148cb63be878ffcc083bde
maxmills <8balldigitalsolutions@gmail.com>
2020-07-31 11:22:23 +0100

    update typhoeus and vcr

--- config/initializers/typhoeus.rb
+++ config/initializers/typhoeus.rb
@@ -1,6 +1,3 @@
-# takes   options = { default_ttl: integer }
-
-Typhoeus.configure do |config|
-  config.cache = Typhoeus::Cache::Rails.new
-  config.instance_variable_set(:@default_ttl, 5)
-end
+Typhoeus::Config.cache = Typhoeus::Cache::Rails.new
+# temporarily allow updated requests as not respondng to etags
+Typhoeus::Config.cache.instance_variable_set(:@default_ttl, 5)
--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,6 +2,9 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/vcr"
   config.hook_into :typhoeus
   config.configure_rspec_metadata!
+  config.before_record do |i|
+    i.response.body.force_encoding('UTF-8')
+  end
   Rails.application.credentials.config.each do |k, v|
     config.filter_sensitive_data("ENV[#{k}]") { v }
   end
