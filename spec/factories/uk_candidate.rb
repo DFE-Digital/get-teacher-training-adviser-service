938e82f33f7bb930083553a118c323c9d18ab2fa
maxmills <8balldigitalsolutions@gmail.com>
2020-08-12 15:07:12 +0100

    update address_line

--- spec/factories/uk_candidate.rb
+++ spec/factories/uk_candidate.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :uk_candidate do
-    address_line_1 { "144" }
-    address_line_2 { "Johnson Avenue" }
+    address_line1 { "144" }
+    address_line2 { "Johnson Avenue" }
     address_city { "Liverpool" }
     address_postcode { "tr1 1uf" }
   end
--- spec/models/degree/uk_candidate_spec.rb
+++ spec/models/degree/uk_candidate_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Degree::UkCandidate do
   let(:address) { build(:degree_uk_candidate) }
-  let(:no_address_line_1) { build(:degree_uk_candidate, address_line_1: "") }
+  let(:no_address_line1) { build(:degree_uk_candidate, address_line1: "") }
 
   describe "#next_step" do
     it "returns the next step" do
