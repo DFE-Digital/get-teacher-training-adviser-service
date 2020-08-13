6b81d75f9d88836ed920c10154d79a27dfb97eaf
maxmills <8balldigitalsolutions@gmail.com>
2020-08-13 12:33:26 +0100

    update returning teacher

--- spec/models/returning_teacher_spec.rb
+++ spec/models/returning_teacher_spec.rb
@@ -20,6 +20,13 @@ RSpec.describe ReturningTeacher do
     end
   end
 
+  describe "#set_education_phase" do
+    it "sets the preferred_education_phase_id before validation" do
+      returning_teacher.valid?
+      expect(returning_teacher.preferred_education_phase_id).to eq(StageInterestedTeaching::OPTIONS[:secondary].to_i)
+    end
+  end
+
   describe "#next_step" do
     context "when answer is true" do
       it "returns the correct option" do
