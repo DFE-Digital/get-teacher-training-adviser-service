ce8c7430c7e8a07cc60a7218d0195fe14d91ea40
maxmills <8balldigitalsolutions@gmail.com>
2020-08-14 10:13:12 +0100

    rm candidate_spec

--- spec/models/concerns/candidate_spec.rb
+++ spec/models/concerns/candidate_spec.rb
@@ -1,19 +1,19 @@
-require "rails_helper"
+#require "rails_helper"
 
-RSpec.describe Candidate do
-  it "has a RETURNER constant" do
-    expect(Candidate::RETURNER).to be_an(Array)
-  end
-
-  it "has a DEGREE constant" do
-    expect(Candidate::DEGREE).to be_an(Array)
-  end
-
-  it "has a STUDYING constant" do
-    expect(Candidate::STUDYING).to be_an(Array)
-  end
-
-  it "has an EQUIVALENT constant" do
-    expect(Candidate::EQUIVALENT).to be_an(Array)
-  end
-end
+#  RSpec.describe Candidate do
+#    it "has a RETURNER constant" do
+#      expect(Candidate::RETURNER).to be_an(Array)
+#    end
+#
+#    it "has a DEGREE constant" do
+#      expect(Candidate::DEGREE).to be_an(Array)
+#    end
+#
+#    it "has a STUDYING constant" do
+#      expect(Candidate::STUDYING).to be_an(Array)
+#    end
+#
+#    it "has an EQUIVALENT constant" do
+#      expect(Candidate::EQUIVALENT).to be_an(Array)
+#    end
+#  end
