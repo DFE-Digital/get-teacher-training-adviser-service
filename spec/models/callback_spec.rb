4fa323838e5ec9a41e17bfb0be3dccdf54d109c2
maxmills <8balldigitalsolutions@gmail.com>
2020-08-17 11:08:30 +0100

    linting

--- spec/models/callback_spec.rb
+++ spec/models/callback_spec.rb
@@ -49,12 +49,11 @@ RSpec.describe Callback, :vcr do
   describe "#self.next_day_check" do
     let(:options) do
       { Date.today.strftime("%a %d %B") => [
-          ["todays times and dates"],["some more todays times and dates"]
-        ],
+        ["todays times and dates"], ["some more todays times and dates"]
+      ],
         Date.tomorrow.strftime("%a %d %B") => [
-          ["tomorrows times and dates"],["some more tomorrows times and dates"]
-        ]
-      }
+          ["tomorrows times and dates"], ["some more tomorrows times and dates"]
+        ] }
     end
     let(:options_hash) { described_class.next_day_check(options) }
     it "removes todays data" do
