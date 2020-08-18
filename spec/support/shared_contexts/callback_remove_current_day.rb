160a8ecb1fad3d18314314af4bae7e9e277d1ff7
maxmills <8balldigitalsolutions@gmail.com>
2020-08-18 12:37:40 +0100

    replace Date with Time.zone

--- spec/support/shared_contexts/callback_remove_current_day.rb
+++ spec/support/shared_contexts/callback_remove_current_day.rb
@@ -1,9 +1,9 @@
 RSpec.shared_context "callback_remove_current_day" do
   let(:options) do
-    { Date.today.strftime("%a %d %B") => [
+    { Time.zone.today.strftime("%a %d %B") => [
       ["todays times and dates"], ["some more todays times and dates"]
     ],
-      Date.tomorrow.strftime("%a %d %B") => [
+      Time.zone.tomorrow.strftime("%a %d %B") => [
         ["tomorrows times and dates"], ["some more tomorrows times and dates"]
       ] }
   end
