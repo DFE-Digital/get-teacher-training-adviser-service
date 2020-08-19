532841f3bc28ca77d53c94a2d7cef22996014d5d
Ross Oliver <ross.oliver36@gmail.com>
2020-08-19 10:52:10 +0100

    Add test coverage for RegistrationsController

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -21,10 +21,11 @@ RSpec.describe RegistrationsController, type: :request do
   end
 
   describe "post /registrations/:step_name" do
-    context "with a valid step name" do
+    context "with a valid step name and payload" do
       it "returns a success response" do
-        post registrations_path(identity.step_name)
-        expect(response).to have_http_status(200)
+        params = { identity: { email: "email@address.com", first_name: "first", last_name: "last" } }
+        post registrations_path(identity.step_name), params: params
+        expect(response).to redirect_to(new_registration_path(identity.next_step))
       end
     end
 
@@ -33,6 +34,14 @@ RSpec.describe RegistrationsController, type: :request do
         expect {
           post registrations_path(invalid_step)
         }.to raise_error(StepFactory::NameNotFoundError)
+      end
+    end
+
+    context "with invalid registration data" do
+      it "redirects to new_registration_path" do
+        params = { identity: { email: "invalid-email.com" } }
+        post registrations_path(identity.step_name), params: params
+        expect(response).to_not redirect_to(new_registration_path(identity.next_step))
       end
     end
   end
