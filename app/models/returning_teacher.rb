443b543ced0113cdcf23d750f2580c3f10934f18
maxmills <8balldigitalsolutions@gmail.com>
2020-08-19 11:48:10 +0100

    update returning_teacher with default attributes

--- app/models/returning_teacher.rb
+++ app/models/returning_teacher.rb
@@ -1,22 +1,11 @@
 class ReturningTeacher < Base
-  attribute :returning_to_teaching, :boolean
-  attribute :degree_options, :string
-  attribute :preferred_education_phase_id, :integer
-
   DEGREE_OPTIONS = { returner: "returner" }.freeze
 
-  before_validation :set_degree_options
-  before_validation :set_education_phase
+  attribute :returning_to_teaching, :boolean
+  attribute :degree_options, :string, default: -> { DEGREE_OPTIONS[:returner] }
+  attribute :preferred_education_phase_id, :integer, default: -> { StageInterestedTeaching::OPTIONS[:secondary].to_i }
 
   validates :returning_to_teaching, inclusion: { in: [true, false], message: "You must select either yes or no" }
-
-  def set_degree_options
-    self.degree_options = DEGREE_OPTIONS[:returner]
-  end
-
-  def set_education_phase
-    self.preferred_education_phase_id = StageInterestedTeaching::OPTIONS[:secondary].to_i
-  end
 
   def next_step
     if returning_to_teaching == true
