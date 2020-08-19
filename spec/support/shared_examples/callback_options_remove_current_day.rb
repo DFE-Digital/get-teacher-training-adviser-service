5339f2790b23fb23a7239908453de40e9c27dc87
maxmills <8balldigitalsolutions@gmail.com>
2020-08-19 09:42:39 +0100

    rm unneeded shared tests

--- spec/support/shared_examples/callback_options_remove_current_day.rb
+++
@@ -1,6 +0,0 @@
-RSpec.shared_examples "callback_options_remove_current_day" do
-  it "removes todays data" do
-    expect(options_hash.size).to eq(1)
-    expect(options_hash.keys).to eq([Date.tomorrow.strftime("%a %d %B")])
-  end
-end
