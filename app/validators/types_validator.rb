6aaa612c8001bd1c5858308c9b57d0f5e0112352
Ross Oliver <ross.oliver36@gmail.com>
2020-08-22 14:00:55 +0100

    Decouple from the ApiClient
    The ApiClient will be removed along with the existing journey; instead
    we will call the GetIntoTeachingApiClient directly (adding the
    ApiClient is a needless abstraction; originally we thought it may do
    more, such as caching but that will be done in the gem itself going
    forward).
    
    It is being decoupled in this commit from any classes/modules that will
    remain in the app once the old registrations flow is removed.

--- app/validators/types_validator.rb
+++ app/validators/types_validator.rb
@@ -1,6 +1,6 @@
 class TypesValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    types = ApiClient.send(options[:method])
+    types = GetIntoTeachingApiClient::TypesApi.new.send(options[:method])
 
     unless types.map { |type| type.id.to_s }.include?(value.to_s)
       record.errors[attribute] << (options[:message] || "is not included in the list")
