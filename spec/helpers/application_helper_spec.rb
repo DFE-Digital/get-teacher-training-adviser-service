0ed2f51163f3f6dafe944d6dfff33df0cccead28
Ross Oliver <ross.oliver36@gmail.com>
2020-08-22 14:19:39 +0100

    Add controller/views for the wizard
    Introduce the `StepsController` to control the Teacher Training Adviser
    wizard, along with the required views for `show`/`update`/`completed`.
    
    Add feature tests for the core journeys and request specs to ensure it
    has the `WizardSteps` concern.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -54,4 +54,31 @@ RSpec.describe ApplicationHelper do
       it { is_expected.to have_css "body.homepage" }
     end
   end
+
+  describe "#govuk_form_for" do
+    it "renders a form with GOV.UK form builder" do
+      expect(govuk_form_for(StubModel.new, url: "http://test.com") {}).to eq(
+        "<form class=\"new_stub_model\" id=\"new_stub_model\" novalidate=\"novalidate\" "\
+        "action=\"http://test.com\" accept-charset=\"UTF-8\" method=\"post\"></form>",
+      )
+    end
+  end
+
+  describe "#back_link" do
+    it "renders a back link with GOV.UK class names" do
+      expect(back_link).to eq("<a class=\"govuk-back-link\" href=\"javascript:history.back()\">Back</a>")
+    end
+  end
+
+  describe "#link_to_change_answer" do
+    it "returns a link to the sign up step" do
+      expect(link_to_change_answer(TeacherTrainingAdviser::Steps::Identity)).to eq(
+        "<a href=\"/teacher_training_adviser/sign_up/identity\">Change</a>",
+      )
+    end
+  end
+
+  class StubModel
+    include ActiveModel::Model
+  end
 end
