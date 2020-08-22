bff8fb5ae6ec526ba9973ae13a7fd6774595fd40
Ross Oliver <ross.oliver36@gmail.com>
2020-08-22 13:53:06 +0100

    Add core wizard classes from GiT app
    We are going to transition the TTA sign up to use the same
    format/classes as the GiT app mailing list/event wizards. This commit
    adds the base/core classes for the Wizard, Store and Step models.
    
    We should look to move these classes into a gem going forward so that
    we can reuse them between both apps.

---
+++ app/controllers/concerns/wizard_steps.rb
@@ -0,0 +1,59 @@
+module WizardSteps
+  extend ActiveSupport::Concern
+
+  included do
+    class_attribute :wizard_class
+    before_action :load_wizard, :load_current_step, except: %i[index completed]
+  end
+
+  def index
+    redirect_to step_path(wizard_class.first_key)
+  end
+
+  def show
+    # current_step loaded via before_action
+  end
+
+  def update
+    @current_step.assign_attributes step_params
+
+    if @current_step.save
+      redirect_to next_step_path
+
+      # Needs to occur after redirect because it purges data after submission
+      @wizard.complete!
+    end
+  end
+
+  def completed
+    # current_step is loaded via before_action
+  end
+
+private
+
+  def load_wizard
+    @wizard = wizard_class.new(wizard_store, params[:id])
+  end
+
+  def load_current_step
+    @current_step = @wizard.find_current_step
+  end
+
+  def next_step_path
+    if (next_key = @wizard.next_key)
+      step_path next_key
+    elsif (invalid_step = @wizard.first_invalid_step)
+      step_path invalid_step
+    else # all steps valid so completed
+      step_path :completed
+    end
+  end
+
+  def step_params
+    params.require(step_param_key).permit @current_step.attributes.keys
+  end
+
+  def step_param_key
+    @current_step.class.model_name.param_key
+  end
+end
