81ccf4f5235dbf724184ef79e40592689ba548ca
Ross Oliver <ross.oliver36@gmail.com>
2020-08-22 13:51:34 +0100

    Prevent overriding session store key in test environment
    In the test environment if the session store is overriden it causes
    the Capybara tests to misbehave; essentially clearing the session
    between each request (resulting in the sign up flows not working as the
    order depends on session data from previous steps). I'm not entirely
    sure why yet; there may be a better way to fix the issue by configuring
    Capybara.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
+# Changing the session store causes it
+# to become nil between Capybara requests.
+return if Rails.env.test?
+
 Rails.application.config.session_store :cache_store,
                                        key: "_#{Rails.application.class.parent_name}_session",
                                        expire_after: 1.day
