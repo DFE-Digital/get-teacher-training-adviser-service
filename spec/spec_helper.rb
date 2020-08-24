aa47d84b564649264adb7d3b6a97c8c9662f15a4
jebw <jeb@jdwilkins.co.uk>
2020-08-24 15:17:06 +0100

    Changed default RSpec logger back to default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,10 +21,6 @@ end
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  # Adding formatter
-  config.formatter = "documentation"
-  config.add_formatter("RspecSonarqubeFormatter", "out/test-report.xml")
-
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
