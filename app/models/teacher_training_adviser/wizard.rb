caeb8ba769b2d3c0e8bd4ea5787a8bc09e457275
Ross Oliver <ross.oliver36@gmail.com>
2020-08-25 14:49:17 +0100

    Submit payload to API at end of sign up
    When the user completes the sign up we bundle the information they entered into
    a request that sends it to the API, where it gets queued before going on to the
    CRM.

--- app/models/teacher_training_adviser/wizard.rb
+++ app/models/teacher_training_adviser/wizard.rb
@@ -1,5 +1,7 @@
 module TeacherTrainingAdviser
   class Wizard < ::Wizard::Base
+    include ::Wizard::ApiClientSupport
+
     self.steps = [
       Steps::Identity,
       Steps::Authenticate,
@@ -38,8 +40,17 @@ module TeacherTrainingAdviser
       super.tap do |result|
         break unless result
 
+        sign_up_candidate
         @store.purge!
       end
+    end
+
+  private
+
+    def sign_up_candidate
+      request = GetIntoTeachingApiClient::TeacherTrainingAdviserSignUp.new(export_camelized_hash)
+      api = GetIntoTeachingApiClient::TeacherTrainingAdviserApi.new
+      api.sign_up_teacher_training_adviser_candidate(request)
     end
   end
 end
