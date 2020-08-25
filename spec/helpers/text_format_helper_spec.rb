23a0a09ffa58faae7e66636268b415e0ff3e9de9
Ross Oliver <ross.oliver36@gmail.com>
2020-08-25 13:05:35 +0100

    Rename PrivacyPolicyFormatHelper to TextFormatHelper
    The `safe_format` method will be used when rendering out answers (mainly for
    spitting out a multiline address without resorting to `html-safe`).

--- spec/helpers/text_format_helper_spec.rb
+++ spec/helpers/text_format_helper_spec.rb
@@ -1,11 +1,12 @@
 require "rails_helper"
 
-RSpec.describe PrivacyPolicyFormatHelper, type: :helper do
+RSpec.describe TextFormatHelper, type: :helper do
   describe "#safe_format" do
-    subject { safe_format content }
+    subject { safe_format(content) }
 
     context "with new lines" do
       let(:content) { "hello\r\n\r\nworld" }
+
       it "wraps content as paragraph" do
         expect(subject).to eq("<p>hello</p>\n\n<p>world</p>")
       end
@@ -13,8 +14,19 @@ RSpec.describe PrivacyPolicyFormatHelper, type: :helper do
 
     context "with html content" do
       let(:content) { "<strong>hello</strong> <em>world</em>" }
+
       it "removes tags and wraps content as paragraph" do
         expect(subject).to eq("<p>hello world</p>")
+      end
+    end
+
+    context "with a custom wrapper tag" do
+      subject { safe_format(content, wrapper_tag: tag) }
+      let(:content) { "hello\r\n\r\nworld" }
+      let(:tag) { "span" }
+
+      it "wraps content in tag" do
+        expect(subject).to eq("<span>hello</span>\n\n<span>world</span>")
       end
     end
   end
