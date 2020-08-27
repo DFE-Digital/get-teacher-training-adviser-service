939ff39f3f8e1e17819b46df9eccdead708f113c
maxmills <8balldigitalsolutions@gmail.com>
2020-08-27 19:32:50 +0100

    lint

--- spec/helpers/what_degree_class_helper_spec.rb
+++ spec/helpers/what_degree_class_helper_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe WhatDegreeClassHelper, type: :helper do
   describe "#remove_third_and_pass_unknown" do
     it "removes third class and pass unknown values" do
       result = remove_third_and_grade_unknown(class_options)
-      expect(result.map{ |x| x.value }).to eq(["Not applicable", "First class", "2:1", "2:2"])
+      expect(result.map { |x| x.value }).to eq(["Not applicable", "First class", "2:1", "2:2"])
     end
   end
 end
