fbe42901fe3ab49afcc4290d7ac23011200a5398
maxmills <8balldigitalsolutions@gmail.com>
2020-08-27 23:11:18 +0100

    lint

--- app/helpers/what_degree_class_helper.rb
+++ app/helpers/what_degree_class_helper.rb
@@ -1,5 +1,5 @@
 module WhatDegreeClassHelper
   def remove_third_and_grade_unknown(degree_class_options)
-    degree_class_options.reject!{ |k| ["Third class or below", "Pass (grade unknown)"].include? k.value }
+    degree_class_options.reject! { |k| ["Third class or below", "Pass (grade unknown)"].include? k.value }
   end
 end
