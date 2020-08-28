c597d29d8b5440ea8bca1522b9cfb3d299a67c4b
maxmills <8balldigitalsolutions@gmail.com>
2020-08-28 17:44:57 +0100

    lint

--- app/models/teacher_training_adviser/steps/what_degree_class.rb
+++ app/models/teacher_training_adviser/steps/what_degree_class.rb
@@ -26,12 +26,10 @@ module TeacherTrainingAdviser::Steps
       @store["degree_options"] == TeacherTrainingAdviser::Steps::HaveADegree::DEGREE_OPTIONS[:studying]
     end
 
-
     def reviewable_answers
       super.tap do |answers|
         answers["uk_degree_grade_id"] = self.class.options.key(uk_degree_grade_id)
       end
     end
-
   end
 end
