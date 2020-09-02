50b87e675df4a83f7a18797c428465697e91cfc6
maxmills <8balldigitalsolutions@gmail.com>
2020-09-02 15:59:43 +0100

    lint

--- app/helpers/callback_helper.rb
+++ app/helpers/callback_helper.rb
@@ -1,15 +1,15 @@
 module CallbackHelper
-  def format_booking_quotas(quotas_by_day, time_zone = "GMT")
-    result =  GetIntoTeachingApiClient::CallbackBookingQuotasApi.new.get_callback_booking_quotas
-      arr = []
-      result.each do |r|
-        r.start_at = r.start_at.in_time_zone(time_zone)
-        arr << r
-      end
-      arr
-      barr = arr.group_by(&:day).reject do |day|
-        Date.parse(day) == Time.zone.today
-      end
+  def format_booking_quotas(_quotas_by_day, time_zone = "GMT")
+    result = GetIntoTeachingApiClient::CallbackBookingQuotasApi.new.get_callback_booking_quotas
+    arr = []
+    result.each do |r|
+      r.start_at = r.start_at.in_time_zone(time_zone)
+      arr << r
+    end
+    arr
+    barr = arr.group_by(&:day).reject do |day|
+      Date.parse(day) == Time.zone.today
+    end
 
     barr.transform_values do |quotas|
       quotas.map do |quota|
