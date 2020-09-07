e9b227aa0eae8e1ff6b3ad1c55e1be88ff88d10f
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2020-09-07 16:20:04 +0100

    have science GCSE

--- app/models/teacher_training_adviser/steps/gcse_science.rb
+++ app/models/teacher_training_adviser/steps/gcse_science.rb
@@ -2,7 +2,7 @@ module TeacherTrainingAdviser::Steps
   class GcseScience < Wizard::Step
     attribute :has_gcse_science_id, :integer
 
-    validates :has_gcse_science_id, types: { method: :get_candidate_retake_gcse_status, message: "You must select either yes or no" }
+    validates :has_gcse_science_id, types: { method: :get_candidate_retake_gcse_status, message: "Select yes if you have Science GCSE or equivalent" }
 
     OPTIONS = Crm::OPTIONS
 
