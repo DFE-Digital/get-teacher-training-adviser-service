0e2c8ed55448ee2879cd07cfb430986c186bc13f
Ross Oliver <ross.oliver36@gmail.com>
2020-09-08 08:05:31 +0100

    Make use of today? helper method

--- app/models/concerns/callback_booking_quotas.rb
+++ app/models/concerns/callback_booking_quotas.rb
@@ -2,7 +2,7 @@ module CallbackBookingQuotas
   def callback_booking_quotas
     quotas = GetIntoTeachingApiClient::CallbackBookingQuotasApi.new.get_callback_booking_quotas
     quotas.reject do |quota|
-      quota.start_at.in_time_zone.to_date == Time.zone.now.to_date
+      quota.start_at.in_time_zone.today?
     end
   end
 end
