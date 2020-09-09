98392a68b5450f4490273c5c95dc79751bddbe30
Craig Taylor <craig.taylor@sevenwhitepixels.co.uk>
2020-09-09 09:24:14 +0100

    remove not needed route from routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,4 @@ Rails.application.routes.draw do
   get "/404", to: "errors#not_found", via: :all
   get "/422", to: "errors#unprocessable_entity", via: :all
   get "/500", to: "errors#internal_server_error", via: :all
-
-  # This route should remain at the bottom to avoid overriding the above routes
-  get "/:page", to: "pages#show"
 end
