18f726d147f2f5161b46d58ccbc29fd8d607e7e7
jebw <jeb@jdwilkins.co.uk>
2020-09-09 17:43:18 +0100

    Added cookie preferences page

--- spec/requests/cookie_preferences_controller_spec.rb
+++ spec/requests/cookie_preferences_controller_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe CookiePreferencesController do
   describe "#show" do
     before { get cookie_preference_path }
     it { is_expected.to have_http_status :success }
-    it { expect(subject.body).to match "Edit your cookie settings" }
+    it { expect(subject.body).to match "Cookie settings" }
   end
 end
