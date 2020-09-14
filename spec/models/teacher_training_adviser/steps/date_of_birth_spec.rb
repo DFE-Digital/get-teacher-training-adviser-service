e682a8206d7e3fee48b6a23dfb43d95f6e38918d
Ross Oliver <ross.oliver36@gmail.com>
2020-09-14 08:17:59 +0100

    Custom error for invalid date of birth
    Previously when an invalid `date_of_birth` was set we were showing the
    validation error associated with the `presence` validation (as we rescue and
    `nil` the `date_of_birth` if it can't be parsed as a `Date`).
    
    Instead, this commit will add a custom error message when we rescue from an
    invalid date format and inform the user that they did not enter a valid date of
    birth.
    
    Clean up `DateOfBirth` model, specifically around attribute assignment in
    `date_of_birth`.

--- spec/models/teacher_training_adviser/steps/date_of_birth_spec.rb
+++ spec/models/teacher_training_adviser/steps/date_of_birth_spec.rb
@@ -8,24 +8,22 @@ RSpec.describe TeacherTrainingAdviser::Steps::DateOfBirth do
 
   context "attributes" do
     it { is_expected.to respond_to :date_of_birth }
-    it { is_expected.to respond_to "date_of_birth(3i)" }
-    it { is_expected.to respond_to "date_of_birth(2i)" }
-    it { is_expected.to respond_to "date_of_birth(1i)" }
   end
 
-  describe "date_of_birth" do
+  describe "#date_of_birth" do
     it { is_expected.to_not allow_value(nil).for :date_of_birth }
     it { is_expected.to_not allow_value(Date.new(1900, 1, 1)).for :date_of_birth }
     it { is_expected.to_not allow_value(1.year.from_now).for :date_of_birth }
+    it { is_expected.to_not allow_value(13.years.ago).for :date_of_birth }
     it { is_expected.to allow_value(18.years.ago).for :date_of_birth }
-  end
 
-  it "maps individual components to date_of_birth" do
-    subject.send("date_of_birth(1i)=", 2001)
-    subject.send("date_of_birth(2i)=", 4)
-    subject.send("date_of_birth(3i)=", 20)
-    subject.valid?
-    expect(subject.date_of_birth).to eq(Date.new(2001, 4, 20))
+    context "when validating" do
+      it "adds a custom error when an invalid date is entered" do
+        subject.date_of_birth = { 3 => -1, 2 => -1, 1 => -1 }
+        expect(subject).to_not be_valid
+        expect(subject.errors[:date_of_birth]).to include("You did not enter a valid date of birth")
+      end
+    end
   end
 
   describe "#reviewable_answers" do
