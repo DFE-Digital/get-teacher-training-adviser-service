5c2ebb8613a1cdaf1e72a414d35a602136b5b835
matt <mattonfoot@gmail.com>
2020-09-18 12:33:12 +0100

    booking quotas to file

--- spec/features/candidate_sign_up_spec.rb
+++ spec/features/candidate_sign_up_spec.rb
@@ -6,76 +6,35 @@ Dir.glob("tmp/contracts/candidate_*.json") do |filename|
   contract_fixture_files.push filename
 end
 
-privacy_policy_reponse_file = "tmp/contracts/privacy_policy.json"
-degree_status_reponse_file = "tmp/contracts/degree_status.json"
-teaching_subjects_reponse_file = "tmp/contracts/teaching_subjects.json"
-countries_reponse_file = "tmp/contracts/countries.json"
-uk_degree_grades_reponse_file = "tmp/contracts/uk_degree_grades.json"
-initial_teacher_training_years_reponse_file = "tmp/contracts/initial_teacher_training_years.json"
-retake_gcse_status_reponse_file = "tmp/contracts/retake_gcse_status.json"
-qualification_types_reponse_file = "tmp/contracts/qualification_types.json"
-education_phases_reponse_file = "tmp/contracts/education_phases.json"
-
-# mocked reference data API definitions
-latest_privacy_policy_api = "#{Rails.application.config.x.git_api_endpoint}/api/privacy_policies/latest"
-specific_privacy_policy_api = "#{Rails.application.config.x.git_api_endpoint}/api/privacy_policies/0a203956-e935-ea11-a813-000d3a44a8e9"
-degree_status_api = "#{Rails.application.config.x.git_api_endpoint}/api/types/qualification/degree_status"
-teaching_subjects_api = "#{Rails.application.config.x.git_api_endpoint}/api/types/teaching_subjects"
-countries_api = "#{Rails.application.config.x.git_api_endpoint}/api/types/countries"
-uk_degree_grades_api = "#{Rails.application.config.x.git_api_endpoint}/api/types/qualification/uk_degree_grades"
-education_phases_api = "#{Rails.application.config.x.git_api_endpoint}/api/types/candidate/preferred_education_phases"
-retake_gcse_status_api = "#{Rails.application.config.x.git_api_endpoint}/api/types/candidate/retake_gcse_status"
-initial_teacher_training_years_api = "#{Rails.application.config.x.git_api_endpoint}/api/types/candidate/initial_teacher_training_years"
-qualification_types_api = "#{Rails.application.config.x.git_api_endpoint}/api/types/qualification/types"
-booking_quotas_api = "#{Rails.application.config.x.git_api_endpoint}/api/callback_booking_quotas"
-signup_for_teacher_training_adviser_api_uri = "/api/teacher_training_adviser/candidates"
-signup_for_teacher_training_adviser_api = "#{Rails.application.config.x.git_api_endpoint}#{signup_for_teacher_training_adviser_api_uri}"
-
 unless contract_fixture_files.empty?
-  # load the reference data files for the API responses
-  privacy_policy_body = JSON.parse(File.read(privacy_policy_reponse_file))
-  degree_status_body = JSON.parse(File.read(degree_status_reponse_file))
-  teaching_subjects_body = JSON.parse(File.read(teaching_subjects_reponse_file))
-  countries_body = JSON.parse(File.read(countries_reponse_file))
-  uk_degree_grades_body = JSON.parse(File.read(uk_degree_grades_reponse_file))
-  education_phases_body = JSON.parse(File.read(education_phases_reponse_file))
-  retake_gcse_status_body = JSON.parse(File.read(retake_gcse_status_reponse_file))
-  initial_teacher_training_years_body = JSON.parse(File.read(initial_teacher_training_years_reponse_file))
-  qualification_types_body = JSON.parse(File.read(qualification_types_reponse_file))
-end
+  # mocked reference data API definitions
+  latest_privacy_policy_api = "#{Rails.application.config.x.git_api_endpoint}/api/privacy_policies/latest"
+  specific_privacy_policy_api = "#{Rails.application.config.x.git_api_endpoint}/api/privacy_policies/0a203956-e935-ea11-a813-000d3a44a8e9"
+  degree_status_api = "#{Rails.application.config.x.git_api_endpoint}/api/types/qualification/degree_status"
+  teaching_subjects_api = "#{Rails.application.config.x.git_api_endpoint}/api/types/teaching_subjects"
+  countries_api = "#{Rails.application.config.x.git_api_endpoint}/api/types/countries"
+  uk_degree_grades_api = "#{Rails.application.config.x.git_api_endpoint}/api/types/qualification/uk_degree_grades"
+  education_phases_api = "#{Rails.application.config.x.git_api_endpoint}/api/types/candidate/preferred_education_phases"
+  retake_gcse_status_api = "#{Rails.application.config.x.git_api_endpoint}/api/types/candidate/retake_gcse_status"
+  initial_teacher_training_years_api = "#{Rails.application.config.x.git_api_endpoint}/api/types/candidate/initial_teacher_training_years"
+  qualification_types_api = "#{Rails.application.config.x.git_api_endpoint}/api/types/qualification/types"
+  booking_quotas_api = "#{Rails.application.config.x.git_api_endpoint}/api/callback_booking_quotas"
 
-booking_quotas_body = [
-  {
-    "timeSlot": "10am - 10:30am",
-    "day": "Tuesday 21 July",
-    "startAt": "2020-07-21T09:00:00Z",
-    "endAt": "2020-07-21T09:30:00Z",
-    "numberOfBookings": 0,
-    "quota": 12,
-    "isAvailable": true,
-    "id": "d828803b-1c83-ea11-a811-000d3a44a94a",
-  },
-  {
-    "timeSlot": "10:30am - 11am",
-    "day": "Tuesday 21 July",
-    "startAt": "2020-07-21T09:30:00Z",
-    "endAt": "2020-07-21T10:00:00Z",
-    "numberOfBookings": 0,
-    "quota": 12,
-    "isAvailable": true,
-    "id": "da28803b-1c83-ea11-a811-000d3a44a94a",
-  },
-  {
-    "timeSlot": "11am - 11:30am",
-    "day": "Tuesday 21 July",
-    "startAt": "2020-07-21T10:00:00Z",
-    "endAt": "2020-07-21T10:30:00Z",
-    "numberOfBookings": 0,
-    "quota": 12,
-    "isAvailable": true,
-    "id": "dc28803b-1c83-ea11-a811-000d3a44a94a",
-  },
-]
+  signup_for_teacher_training_adviser_api_uri = "/api/teacher_training_adviser/candidates"
+  signup_for_teacher_training_adviser_api = "#{Rails.application.config.x.git_api_endpoint}#{signup_for_teacher_training_adviser_api_uri}"
+
+  # load the reference data files for the API responses
+  privacy_policy_body = JSON.parse(File.read("tmp/contracts/privacy_policy.json"))
+  degree_status_body = JSON.parse(File.read("tmp/contracts/degree_status.json"))
+  teaching_subjects_body = JSON.parse(File.read("tmp/contracts/teaching_subjects.json"))
+  countries_body = JSON.parse(File.read("tmp/contracts/countries.json"))
+  uk_degree_grades_body = JSON.parse(File.read("tmp/contracts/uk_degree_grades.json"))
+  education_phases_body = JSON.parse(File.read("tmp/contracts/education_phases.json"))
+  retake_gcse_status_body = JSON.parse(File.read("tmp/contracts/retake_gcse_status.json"))
+  initial_teacher_training_years_body = JSON.parse(File.read("tmp/contracts/initial_teacher_training_years.json"))
+  qualification_types_body = JSON.parse(File.read("tmp/contracts/qualification_types.json"))
+  booking_quotas_body = JSON.parse(File.read("tmp/contracts/booking_quotas.json"))
+end
 
 def yes_no_options(option)
   case option
@@ -99,7 +58,7 @@ def primary_secondary_options(option)
   end
 end
 
-# step definitions
+# Begin Step definitions
 
 def start_sign_up_wizard
   visit "/"
@@ -445,66 +404,32 @@ def accept_the_privacy_policy
   click_button "Continue"
 end
 
+# end Step definitions
+
+def add_stub(uri, response_body)
+  stub_request(:get, uri)
+    .to_return(body: JSON.generate(response_body), headers: {
+      "content-type": "application/json; charset=utf-8",
+    })
+end
+
 RSpec.feature "Dependency contracts", :vcr, type: :feature do
   include WebMock::API
 
   before do
     WebMock.enable!
 
-    stub_request(:get, latest_privacy_policy_api)
-      .to_return(body: JSON.generate(privacy_policy_body), headers: {
-        "content-type": "application/json; charset=utf-8",
-      })
-
-    stub_request(:get, specific_privacy_policy_api)
-      .to_return(body: JSON.generate(privacy_policy_body), headers: {
-        "content-type": "application/json; charset=utf-8",
-      })
-
-    stub_request(:get, degree_status_api)
-    .to_return(body: JSON.generate(degree_status_body), headers: {
-      "content-type": "application/json; charset=utf-8",
-    })
-
-    stub_request(:get, teaching_subjects_api)
-      .to_return(body: JSON.generate(teaching_subjects_body), headers: {
-        "content-type": "application/json; charset=utf-8",
-      })
-
-    stub_request(:get, countries_api)
-      .to_return(body: JSON.generate(countries_body), headers: {
-        "content-type": "application/json; charset=utf-8",
-      })
-
-    stub_request(:get, uk_degree_grades_api)
-      .to_return(body: JSON.generate(uk_degree_grades_body), headers: {
-        "content-type": "application/json; charset=utf-8",
-      })
-
-    stub_request(:get, education_phases_api)
-      .to_return(body: JSON.generate(education_phases_body), headers: {
-        "content-type": "application/json; charset=utf-8",
-      })
-
-    stub_request(:get, retake_gcse_status_api)
-      .to_return(body: JSON.generate(retake_gcse_status_body), headers: {
-        "content-type": "application/json; charset=utf-8",
-      })
-
-    stub_request(:get, initial_teacher_training_years_api)
-      .to_return(body: JSON.generate(initial_teacher_training_years_body), headers: {
-        "content-type": "application/json; charset=utf-8",
-      })
-
-    stub_request(:get, qualification_types_api)
-      .to_return(body: JSON.generate(qualification_types_body), headers: {
-        "content-type": "application/json; charset=utf-8",
-      })
-
-    stub_request(:get, booking_quotas_api)
-      .to_return(body: JSON.generate(booking_quotas_body), headers: {
-        "content-type": "application/json; charset=utf-8",
-      })
+    add_stub latest_privacy_policy_api, privacy_policy_body
+    add_stub specific_privacy_policy_api, privacy_policy_body
+    add_stub degree_status_api, degree_status_body
+    add_stub teaching_subjects_api, teaching_subjects_body
+    add_stub countries_api, countries_body
+    add_stub uk_degree_grades_api, uk_degree_grades_body
+    add_stub education_phases_api, education_phases_body
+    add_stub retake_gcse_status_api, retake_gcse_status_body
+    add_stub initial_teacher_training_years_api, initial_teacher_training_years_body
+    add_stub qualification_types_api, qualification_types_body
+    add_stub booking_quotas_api, booking_quotas_body
 
     stub_request(:post, signup_for_teacher_training_adviser_api)
       .to_return(headers: {
@@ -522,17 +447,22 @@ RSpec.feature "Dependency contracts", :vcr, type: :feature do
     data = JSON.parse(File.read(filename))
     data["filename"] = File.basename(filename)
 
-    next if data["Persona"] == "--" || data["Final Journey Status"] != "COMPLETE"
+    fixture_data = data["uiSignUp"]
+
+    next if fixture_data["Persona"] == "--" || fixture_data["Final Journey Status"] != "COMPLETE"
 
     # add the candidate to the correct Persona list
-    personas[data["Persona"]] = personas[data["Persona"]] || []
-    personas[data["Persona"]].push data
+    personas[fixture_data["Persona"]] = personas[fixture_data["Persona"]] || []
+    personas[fixture_data["Persona"]].push data
   end
 
   personas.each do |persona, candidates|
     feature "As a #{persona}" do
-      candidates.each do |candidate|
-        scenario "Someone can sign up for a Teacher Training Advisor with profile defined by [#{candidate['filename']}]" do
+      candidates.each do |fixture|
+        candidate = fixture["uiSignUp"]
+        api_submission = fixture["apiSubmission"]
+
+        scenario "Someone can sign up for a Teacher Training Advisor with profile defined by [#{fixture['filename']}]" do
           start_sign_up_wizard
 
           enter_personal_information candidate["First Name"], candidate["Surname"], candidate["Email"]
@@ -571,7 +501,7 @@ RSpec.feature "Dependency contracts", :vcr, type: :feature do
 
           accept_the_privacy_policy
 
-          expect(request).to eql candidate["apiSubmission"]
+          expect(request).to eql api_submission
         end
       end
     end
