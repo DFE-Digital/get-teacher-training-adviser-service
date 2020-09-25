e62c4ead0b913e4c3bd79db817001e6eb9894a32
Ross Oliver <ross.oliver36@gmail.com>
2020-09-25 08:54:23 +0100

    Filter sensitive information from logs
    We don't want to capture any personally identifiable information in our
    logs by accident.

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,17 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += %i[
+  address_city
+  address_line1
+  address_line2
+  address_postcode
+  date_of_birth
+  email
+  first_name
+  last_name
+  password
+  teacher_id
+  telephone
+  timed_one_time_password
+]
