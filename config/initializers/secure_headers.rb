6205cc0f593343ad3ac8a0262727be13a25dc1e8
jebw <jeb@jdwilkins.co.uk>
2020-10-07 10:49:48 +0100

    Added doubleclick frame support for CSP

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -18,7 +18,7 @@ SecureHeaders::Configuration.default do |config|
     font_src: %w['self' *.gov.uk fonts.gstatic.com],
     form_action: %w['self' tr.snapchat.com www.facebook.com],
     frame_ancestors: %w['self'],
-    frame_src: %w['self' tr.snapchat.com www.facebook.com *.hotjar.com],
+    frame_src: %w['self' tr.snapchat.com www.facebook.com *.hotjar.com *.doubleclick.net],
     img_src: %W['self' *.gov.uk data: *.googleapis.com www.facebook.com ct.pinterest.com t.co www.facebook.com cx.atdmt.com ad.doubleclick.net] + google_analytics,
     manifest_src: %w['self'],
     media_src: %w['self'],
