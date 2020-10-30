a97730e3fa0d7ebd24e73a4d5691a4a9d5cb8d27
Ross Oliver <ross.oliver36@gmail.com>
2020-10-30 09:01:26 +0000

    Rescue ApiError and render 429 error page
    Whilst the app itself is rate limited the API also has rate limiting. In the
    unlikely event that the client rate limits don't trigger and instead the API's
    rate limiting is enforced we want to display the same 429 error page to the
    user.
    
    Raise `ApiError` up to the `ApplicationController` when the `code` is 429. The
    `ApplicationController` will then rescue the error and render the error page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,23 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActionController::InvalidAuthenticityToken, with: :session_expired
   rescue_from ActionController::RoutingError, with: :render_not_found
+  rescue_from GetIntoTeachingApiClient::ApiError, with: :handle_api_error
 
   def raise_not_found
     raise ActionController::RoutingError, "Not Found"
   end
 
 private
+
+  def handle_api_error(error)
+    render_too_many_requests && return if error.code == 429
+
+    raise
+  end
+
+  def render_too_many_requests
+    render template: "errors/too_many_requests", status: :too_many_requests
+  end
 
   def render_not_found
     render template: "errors/not_found", status: :not_found
