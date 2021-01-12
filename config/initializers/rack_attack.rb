78246e983ca0c904e7f5f7e9b9a5ea6f66b8bbef
jebw <jeb@jdwilkins.co.uk>
2021-01-12 11:42:39 +0000

    Only disable Rack::Attack req limits in `preprod`
    We still want the other Rack::Attack restrictions enabled for testing purposes

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -15,21 +15,23 @@ module Rack
       req.ip if req.path == "/csp_reports"
     end
 
-    # Throttle requests that issue a verification code by IP (5rpm)
-    throttle("issue_verification_code req/ip", limit: 5, period: 1.minute) do |req|
-      req.ip if (req.patch? || req.put?) && req.path == "/teacher_training_adviser/sign_up/identity"
-    end
+    unless ENV["SKIP_REQ_LIMITS"].to_s.in? %w[true yes 1]
+      # Throttle requests that issue a verification code by IP (5rpm)
+      throttle("issue_verification_code req/ip", limit: 5, period: 1.minute) do |req|
+        req.ip if (req.patch? || req.put?) && req.path == "/teacher_training_adviser/sign_up/identity"
+      end
 
-    # Throttle requests that resend a verification code by IP (5rpm)
-    throttle("resend_verification_code req/ip", limit: 5, period: 1.minute) do |req|
-      path_resends_verification_code = %r{/*./resend_verification}.match?(req.path)
+      # Throttle requests that resend a verification code by IP (5rpm)
+      throttle("resend_verification_code req/ip", limit: 5, period: 1.minute) do |req|
+        path_resends_verification_code = %r{/*./resend_verification}.match?(req.path)
 
-      req.ip if req.get? && path_resends_verification_code
-    end
+        req.ip if req.get? && path_resends_verification_code
+      end
 
-    # Throttle teacher training adviser sign ups by IP (5rpm)
-    throttle("teacher_training_adviser_sign_up req/ip", limit: 5, period: 1.minute) do |req|
-      req.ip if (req.patch? || req.put?) && req.path == "/teacher_training_adviser/sign_up/accept_privacy_policy"
+      # Throttle teacher training adviser sign ups by IP (5rpm)
+      throttle("teacher_training_adviser_sign_up req/ip", limit: 5, period: 1.minute) do |req|
+        req.ip if (req.patch? || req.put?) && req.path == "/teacher_training_adviser/sign_up/accept_privacy_policy"
+      end
     end
 
     if ENV["FAIL2BAN"].to_s.match? %r{\A\d+\z}
