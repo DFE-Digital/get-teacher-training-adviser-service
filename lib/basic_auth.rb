1a74b0fd7eddfcdafbd41ffcbea90832e22461e3
Ross Oliver <ross.oliver36@gmail.com>
2021-04-20 12:50:04 +0100

    Support multiple basic auth credentials
    Extract the basic auth logic from the controller into a lib; updates to support
    multiple logins, as we will need this going forward (for restricting access to
    the feedback section).
    
    The controller will set a `session[:username]` when auth is successful so that
    we can later scope certain actions to particular users.

---
+++ lib/basic_auth.rb
@@ -0,0 +1,20 @@
+class BasicAuth
+  class << self
+    def authenticate(username, password)
+      return false unless [username, password].all?(&:present?)
+
+      credentials.any? { |c| c[:username] == username && c[:password] == password }
+    end
+
+    def credentials
+      @@credentials ||= http_auth.split(",").map do |credential|
+        parts = credential.split("=")
+        { username: parts.first, password: parts.last }
+      end
+    end
+
+    def http_auth
+      Rails.application.credentials.config[:http_auth]
+    end
+  end
+end
