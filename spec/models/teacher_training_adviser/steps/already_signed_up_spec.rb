d25a3dd9904b7ec38e794e5105055b6c4762dba3
Ross Oliver <ross.oliver36@gmail.com>
2021-07-29 11:44:36 +0100

    Allow some candidates to re-sign up for a TTA
    Previously we were using the
    `already_subscribed_to_teacher_training_adviser` field to determine if
    we prevent the user from continuing with their sign up. Instead, we
    want to use the `can_subscribe_to_teacher_training_adviser` field
    which will allow some candidates that have already subscribed to
    re-subscribe (depending on their adviser status as determined by the
    API).

--- spec/models/teacher_training_adviser/steps/already_signed_up_spec.rb
+++ spec/models/teacher_training_adviser/steps/already_signed_up_spec.rb
@@ -7,17 +7,17 @@ RSpec.describe TeacherTrainingAdviser::Steps::AlreadySignedUp do
   it { is_expected.to_not be_can_proceed }
 
   describe "#skipped?" do
-    it "returns true if already_subscribed_to_teacher_training_adviser is false/nil/undefined" do
-      expect(subject).to be_skipped
-      wizardstore["already_subscribed_to_teacher_training_adviser"] = nil
-      expect(subject).to be_skipped
-      wizardstore["already_subscribed_to_teacher_training_adviser"] = false
-      expect(subject).to be_skipped
+    it "returns false if can_subscribe_to_teacher_training_adviser is false" do
+      wizardstore["can_subscribe_to_teacher_training_adviser"] = false
+      expect(subject).not_to be_skipped
     end
 
-    it "returns false if already_subscribed_to_teacher_training_adviser is true" do
-      wizardstore["already_subscribed_to_teacher_training_adviser"] = true
-      expect(subject).to_not be_skipped
+    it "returns true if can_subscribe_to_teacher_training_adviser is true/nil/undefined" do
+      expect(subject).to be_skipped
+      wizardstore["can_subscribe_to_teacher_training_adviser"] = nil
+      expect(subject).to be_skipped
+      wizardstore["can_subscribe_to_teacher_training_adviser"] = true
+      expect(subject).to be_skipped
     end
   end
 end
