b2233cbe1c190eeeeeaa5a05513197968675abbf
Ross Oliver <ross.oliver36@gmail.com>
2021-10-01 12:02:18 +0100

    Support custom channel in TTA sign up
    The events team are pushing TTA sign ups at on-campus events and want to be
    able to attribute a successful sign up to a particular event. The API now
    accepts a `channe_id` in the TTA sign up request model, so this PR updates the
    TTA root URL to forward on a `channel` attribute, that will be used for the
    `channel_id` when signing up. It is persisted as part of the first (identity)
    step.
    
    To sign up the URL format is:
    
    ```
    /?channel=<channel_id>
    ```

--- spec/models/teacher_training_adviser/steps/identity_spec.rb
+++ spec/models/teacher_training_adviser/steps/identity_spec.rb
@@ -2,6 +2,18 @@ require "rails_helper"
 
 RSpec.describe TeacherTrainingAdviser::Steps::Identity do
   include_context "wizard step"
+  before do
+    allow_any_instance_of(GetIntoTeachingApiClient::PickListItemsApi).to \
+      receive(:get_candidate_teacher_training_adviser_subscription_channels).and_return(channels)
+  end
+
+  let(:channels) do
+    [
+      GetIntoTeachingApiClient::PickListItem.new({ id: 12_345, value: "Channel 1" }),
+      GetIntoTeachingApiClient::PickListItem.new({ id: 67_890, value: "Channel 2" }),
+    ]
+  end
+
   it_behaves_like "a wizard step"
   it_behaves_like "an issue verification code wizard step"
   include_context "sanitize fields", %i[first_name last_name email]
@@ -27,6 +39,14 @@ RSpec.describe TeacherTrainingAdviser::Steps::Identity do
   describe "email" do
     it { is_expected.not_to allow_values(nil, "", "a@#{'a' * 101}.com", "some@thing").for :email }
     it { is_expected.to allow_values("test@test.com", "test%.mctest@domain.co.uk").for :email }
+  end
+
+  describe "validations for channel_id" do
+    let(:options) { channels.map(&:id) }
+
+    it { is_expected.to allow_values(options).for :channel_id }
+    it { is_expected.to allow_value(nil, "").for :channel_id }
+    it { is_expected.not_to allow_value(11_233).for :channel_id }
   end
 
   describe "#reviewable_answers" do
