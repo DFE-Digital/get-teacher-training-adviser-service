d4269fb7489d675b85b7e2e413d8e9a327885d01
Ross Oliver <ross.oliver36@gmail.com>
2021-12-01 11:55:22 +0000

    Disable new GTM container in non-prod environments

--- config/environments/rolling.rb
+++ config/environments/rolling.rb
@@ -4,5 +4,4 @@ require File.expand_path("production.rb", __dir__)
 Rails.application.configure do
   config.x.git_api_endpoint = "https://get-into-teaching-api-dev.london.cloudapps.digital"
   config.x.enable_beta_redirects = false
-  config.x.legacy_tracking_pixels = false
 end
