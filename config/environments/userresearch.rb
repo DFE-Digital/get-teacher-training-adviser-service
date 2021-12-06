377c2af1e5e358c0343064b7a07a72c604d361da
Ross Oliver <ross.oliver36@gmail.com>
2021-12-06 10:58:27 +0000

    Enable new GTM container in non-prod environments

--- config/environments/userresearch.rb
+++ config/environments/userresearch.rb
@@ -4,7 +4,6 @@ require File.expand_path("production.rb", __dir__)
 Rails.application.configure do
   config.x.git_api_endpoint = "https://get-into-teaching-api-dev.london.cloudapps.digital"
   config.x.enable_beta_redirects = false
-  config.x.legacy_tracking_pixels = true
 
   Rack::Attack.enabled = false
 end
