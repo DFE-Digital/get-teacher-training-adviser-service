df2e4a7d8c456307263ed1f0bfeab8d4dc81b788
Ross Oliver <ross.oliver36@gmail.com>
2022-04-08 10:11:27 +0100

    Migrate userresearch environment secrets to Azure
    The userresearch environment uses the preprod keyvault; I'm not sure why it was
    pointing to the dev CRM instance unless the credentials file overrode the CRM
    it points to, but to simplify this I'm just pointing it to the test CRM.

--- config/environments/userresearch.rb
+++ config/environments/userresearch.rb
@@ -2,7 +2,7 @@
 require File.expand_path("production.rb", __dir__)
 
 Rails.application.configure do
-  config.x.git_api_endpoint = "https://get-into-teaching-api-dev.london.cloudapps.digital"
+  config.x.git_api_endpoint = "https://get-into-teaching-api-test.london.cloudapps.digital"
   config.x.enable_beta_redirects = false
 
   Rack::Attack.enabled = false
