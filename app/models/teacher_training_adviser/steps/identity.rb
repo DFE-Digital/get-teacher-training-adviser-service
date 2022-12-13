6ddc88258f4da6bb5156ba67a26e14295bc4c2e1
Ross Oliver <ross.oliver36@gmail.com>
2022-12-13 13:50:23 +0000

    Update wizard gem for new Identity step
    The wizard gem now contains an `Identity` step that
    encapsulates common attributes and functionality;
    we can inherit from it to DRY up this model in the
    adviser service.

--- app/models/teacher_training_adviser/steps/identity.rb
+++ app/models/teacher_training_adviser/steps/identity.rb
@@ -1,39 +1,14 @@
 module TeacherTrainingAdviser::Steps
-  class Identity < GITWizard::Step
-    include ::GITWizard::IssueVerificationCode
-
-    attribute :first_name, :string
-    attribute :last_name, :string
-    attribute :email, :string
+  class Identity < GITWizard::Steps::Identity
     attribute :channel_id, :integer
     attribute :sub_channel_id
-    attribute :accepted_policy_id, :string
-
-    validates :accepted_policy_id, policy: true
-    validates :first_name, presence: true, length: { maximum: 256 }
-    validates :last_name, presence: true, length: { maximum: 256 }
-    validates :email, presence: true, email_format: true
-
-    before_validation :sanitize_input
-
-    def self.contains_personal_details?
-      true
-    end
 
     def export
-      super.except("sub_channel_id")
-    end
-
-    def latest_privacy_policy
-      @latest_privacy_policy ||= GetIntoTeachingApiClient::PrivacyPoliciesApi.new.get_latest_privacy_policy
+      super.without("sub_channel_id")
     end
 
     def reviewable_answers
-      super
-        .tap { |answers|
-          answers["name"] = "#{answers['first_name']} #{answers['last_name']}"
-        }
-        .without("first_name", "last_name", "channel_id", "sub_channel_id", "accepted_policy_id")
+      super.without(%w[channel_id sub_channel_id])
     end
 
     def channel_ids
@@ -54,12 +29,6 @@ module TeacherTrainingAdviser::Steps
 
     def query_channels
       @query_channels ||= GetIntoTeachingApiClient::PickListItemsApi.new.get_candidate_teacher_training_adviser_subscription_channels
-    end
-
-    def sanitize_input
-      self.first_name = first_name.to_s.strip.presence if first_name
-      self.last_name = last_name.to_s.strip.presence if last_name
-      self.email = email.to_s.strip.presence if email
     end
   end
 end
